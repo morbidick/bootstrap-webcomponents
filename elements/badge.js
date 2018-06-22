64828d495b3e5433431af19a8164708e929ce2fa
morbidick <mail@morbi-happens.de>
2018-06-22 23:14:11 +0200

    badges can now be links

--- elements/badge.js
+++ elements/badge.js
@@ -5,6 +5,7 @@ export default class BsBadge extends LitElement {
 	static get properties() {
 		return {
 			theme: String,
+			href: String,
 		}
 	}
 
@@ -13,7 +14,7 @@ export default class BsBadge extends LitElement {
 		this.theme = "secondary";
 	}
 
-	_render() {
+	_render({href}) {
 		return html`
 			<style>
 				:host {
@@ -22,34 +23,38 @@ export default class BsBadge extends LitElement {
 				:host([hidden]) {
 					display: hidden;
 				}
-				:host {
+				a, span {
 					padding: .25em .4em;
 					border-top-left-radius: var(--bs-border-top-left-radius, .25em);
 					border-top-right-radius:	var(--bs-border-top-right-radius, .25em);
 					border-bottom-left-radius: var(--bs-border-bottom-left-radius, .25em);
 					border-bottom-right-radius: var(--bs-border-bottom-right-radius, .25em);
 
-					font-size: 75%;
+					font-size: .75em;
 					font-weight: 700;
 					line-height: 1;
 					text-align: center;
 					white-space: nowrap;
 					vertical-align: baseline;
+					text-decoration: none;
+				}
+				a:hover {
+					filter: brightness(0.85);
 				}
 				${colors.reduce((style, {selector, color, contrast, focusring}) => {
 					return style + `
-						:host(${selector}) {
+						:host(${selector}) > * {
 							background-color: ${color};
 							color: ${contrast};
 						}`
 				}, "")}
-				:host([theme~="pill"]) {
+				:host([theme~="pill"]) > * {
 					padding-right: .6em;
 					padding-left: .6em;
 					border-radius: 10em;
 				}
 			</style>
-			<slot></slot>
+			${href ? html`<a href$=${href}><slot></slot></a>`: html`<span><slot></slot></span>`}
 		`;
 	}
 
