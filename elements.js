6bc52fba89886a28e1bdf87067f94bfb78ad06a2
morbidick <mail@morbi-happens.de>
2019-02-04 23:13:11 +0100

    export the progress element

--- elements.js
+++ elements.js
@@ -2,3 +2,4 @@ export * from './elements/alert.js';
 export * from './elements/badge.js';
 export * from './elements/button-group.js';
 export * from './elements/button.js';
+export * from './elements/progress.js';
